bd4d289c163741dfa38053a1dc414f07ffc149ba
Aurojit Das <aurojitdas@gmail.com>
2024-10-08 22:07:13 +0100

    Added new migrations for the DB

--- Migrations/20241008210636_applciationUser.Designer.cs
+++ Migrations/20241008210636_applciationUser.Designer.cs
@@ -4,6 +4,7 @@ using AppointmentScheduling1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppointmentScheduling1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241008210636_applciationUser")]
+    partial class applciationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
